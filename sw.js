ad5c65cf24be5c9f97ccb03d1427be466d7058f3
Kyle Wang <kylewangtw@github.com>
2026-03-28 14:17:43 +0800

    Bump SW cache to v3 to force update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sudoku-v1';
+const CACHE = 'sudoku-v3';
 const ASSETS = ['./', './index.html', './style.css', './sudoku.js', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
