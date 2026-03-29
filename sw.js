cf4615db344afa36cf35421764b12e3d967b1a8c
Kyle Wang <kylewangtw@github.com>
2026-03-29 21:27:32 +0800

    Responsive layout for iPad/large screens using vmin units

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sudoku-v5';
+const CACHE = 'sudoku-v6';
 const ASSETS = ['./', './index.html', './style.css', './sudoku.js', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
